b51a74f1e4bf15d101b894d40914bf5c89c9f2a1
tsukushibito <yusuke@yusuke-mac.local>
2020-03-27 00:43:42 +0900

    Add to_rs() to elements.py

--- generator/vkxml2rs/elements.py
+++ generator/vkxml2rs/elements.py
@@ -2,10 +2,33 @@ import dataclasses
 from typing import List
 
 
+def c_type_to_rs_type(c_type: str):
+
+    table = {'void': 'c_void',
+             'char': 'i8',
+             'float': 'f32',
+             'double': 'f64',
+             'uint8_t': 'u8',
+             'uint16_t': 'u16',
+             'uint32_t': 'u32',
+             'uint64_t': 'u64',
+             'int32_t': 'i32',
+             'int64_t': 'i64',
+             'size_t': 'size_t',
+             'int': 'i32', }
+
+    return table.get(c_type, 'unknown')
+
+
 @dataclasses.dataclass
 class BaseType:
     name: str
     typedef: str
+
+    def to_rs(self):
+        return ('type ' + self.name
+                + ' = '
+                + c_type_to_rs_type(self.typedef) + ';\n')
 
 
 @dataclasses.dataclass
@@ -13,10 +36,19 @@ class BitMask:
     name: str
     typedef: str
 
+    def to_rs(self):
+        return ('type ' + self.name
+                + ' = '
+                + c_type_to_rs_type(self.typedef) + ';\n')
+
 
 @dataclasses.dataclass
 class Handle:
     name: str
+
+    def to_rs(self):
+        return ('enum ' + self.name + '_t { }\n'
+                + 'type ' + self.name + ' = *mut ' + self.name + ';\n')
 
 
 @dataclasses.dataclass
@@ -31,6 +63,15 @@ class FuncPointer:
     return_type: str
     params: List[Param]
 
+    def to_rs(self):
+        rs = ('type ' + self.name + ' = ' +
+              '::std::option::Option<extern "C" fn(')
+        rs_params = map(lambda p: p.name + ': ' + p.type_, self.params)
+        rs += ', '.join(rs_params)
+        rs += ');\n'
+
+        return rs
+
 
 @dataclasses.dataclass
 class Enum:
@@ -42,6 +83,13 @@ class Enum:
 class Enums:
     name: str
     enums: List[Enum]
+
+    def to_rs(self):
+        rs = ('struct ' + self.name + '(u32);')
+        for e in self.enums:
+            rs += e.name + ': ' + self.name + ' = ' + e.value + ';\n'
+
+        return rs
 
 
 @dataclasses.dataclass
@@ -55,11 +103,27 @@ class Struct:
     name: str
     members: List[Member]
 
+    def to_rs(self):
+        rs = '#[repr(C)]'
+        rs += ('struct ' + self.name + ' {\n')
+        for m in self.members:
+            rs += '    ' + m.name + ': ' + c_type_to_rs_type(m.type_) + ',\n'
+        rs += '}\n'
+        return rs
+
 
 @dataclasses.dataclass
 class Union:
     name: str
     members: List[Member]
+
+    def to_rs(self):
+        rs = '#[repr(C)]'
+        rs += ('union ' + self.name + ' {\n')
+        for m in self.members:
+            rs += '    ' + m.name + ': ' + c_type_to_rs_type(m.type_) + ',\n'
+        rs += '}\n'
+        return rs
 
 
 @dataclasses.dataclass
@@ -67,3 +131,11 @@ class Command:
     name: str
     return_type: str
     params: List[Param]
+
+    def to_rs(self):
+        rs = ('extern "C" fn(')
+        rs_params = map(lambda p: p.name + ': ' + p.type_, self.params)
+        rs += ', '.join(rs_params)
+        rs += ');\n'
+
+        return rs
